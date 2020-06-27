5aafdf841bbbbb4f00187bc10c1768f158d45417
yerin <ghddnjf2901@gmail.com>
2020-06-28 03:56:17 +0900

    contact 레이아웃 완성

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,9 +10,9 @@ const Contact = () => {
     <div>
       <Nav />
       <div className="container">
-        <div class="info">
-          <div class="title">GET IN TOUCH</div>
-          <div class="content">
+        <div className="info">
+          <div className="title">GET IN TOUCH</div>
+          <div className="content">
             Office
             <div>+82-2-888-8350</div>
           </div>
@@ -20,24 +20,55 @@ const Contact = () => {
             Fax
             <div>+82-2-888-8350</div>
           </div>
-          <div class="content"></div>
-          <div class="content">
+          <div className="content"></div>
+          <div className="content">
             Email
             <div>mirimo01212@gmail.com</div>
           </div>
         </div>
 
-        <div class="direction">
-          <div class="title">DIRECTION</div>
-          <div class="content">
+        <div className="direction">
+          <div className="title">DIRECTION</div>
+          <div className="content">
             546, Hoam-ro, Gwanak-gu, Seoul, Republic of Korea
           </div>
           <MirimoMap class="map"></MirimoMap>
         </div>
 
-        <div class="contact">
-          <div class="title">CONTACT US</div>
-          <div class="content"></div>
+        <div className="contact">
+          <div className="title">CONTACT US</div>
+          <div className="content">
+            <div className="email">
+              Email
+              <input
+                size="85"
+                type="text"
+                name="email"
+                placeholder="example@gmail.com"
+              ></input>
+            </div>
+            <hr></hr>
+            <div className="subject">
+              Subject
+              <input
+                size="85"
+                type="text"
+                name="subject"
+                placeholder="Please write the title"
+              ></input>
+            </div>
+            <hr></hr>
+            <div className="message">
+              Message
+              <input
+                size="85"
+                type="text"
+                name="message"
+                placeholder="Detailed information on project content, schedule, etc"
+              ></input>
+            </div>
+            <input className="send" type="submit" value="SEND"></input>
+          </div>
         </div>
       </div>
       <Footer />
