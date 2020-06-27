fd9244966a7b23bf255092b8be3b47582279c80c
KangEunseo <s2018w21@e-mirim.hs.kr>
2020-06-28 00:44:57 +0900

    Done Project Layout

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,7 +9,46 @@ const Project = () => {
         <div>
             <Nav/>
             <div className="project">
-                <div>project</div>
+                <div className="container">
+                    
+                    {/* WORK */}
+                    <div className="about-content">
+                        <div className="content-title">WORK</div>
+                        {/* PROJECT LIST */}
+                        <div className="wrap-project">
+                            <div className="project-item">
+                                <div className="project-title">RAJA</div>
+                                <div className="project-intro">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</div>
+                                <div className="project-detail">View more</div>
+                            </div>
+                            <div className="project-image"></div>
+                        </div>
+                        <div className="wrap-project">
+                            <div className="project-image"></div>
+                            <div className="project-item">
+                                <div className="project-title">JHsustain</div>
+                                <div className="project-intro">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</div>
+                                <div className="project-detail">View more</div>
+                            </div>
+                        </div>
+                        <div className="wrap-project">
+                            <div className="project-item">
+                                <div className="project-title">8gram</div>
+                                <div className="project-intro">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</div>
+                                <div className="project-detail">View more</div>
+                            </div>
+                            <div className="project-image"></div>
+                        </div>
+                        <div className="wrap-project">
+                            <div className="project-image"></div>
+                            <div className="project-item">
+                                <div className="project-title">MirimMediaLab</div>
+                                <div className="project-intro">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</div>
+                                <div className="project-detail">View more</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <Footer/>
         </div>
