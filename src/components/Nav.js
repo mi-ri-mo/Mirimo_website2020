59619716e56b6064d2c166c927b1eeb3468e18e9
yerin <ghddnjf2901@gmail.com>
2020-06-30 23:39:54 +0900

    nav style and active style modified

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../styles/Nav.scss'
+import '../styles/Nav.scss';
 
 const Nav = () => {
-    return(
-        <div>
-            <div className="nav">
-                <div className="container">
-                    <div className="wrap-nav">
-                        <div>
-                            <NavLink exact={true} to="/">
-                                <div className="nav-logo"></div>
-                            </NavLink>
-                        </div>
-                        <div className="nav-menu">
-                            <NavLink exact to="/">About</NavLink>
-                            <NavLink to="/contact">Contact</NavLink>
-                            <NavLink to="/project">Project</NavLink>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div>
+      <div className="nav">
+        <div className="container">
+          <div className="wrap-nav">
+            <div>
+              <NavLink exact={true} to="/">
+                <div className="nav-logo"></div>
+              </NavLink>
             </div>
+            <div className="nav-menu">
+              <NavLink exact to="/">
+                About
+              </NavLink>
+              <NavLink to="/contact" activeClassName="active">
+                Contact
+              </NavLink>
+              <NavLink to="/project" activeClassName="active">
+                Project
+              </NavLink>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Nav;
--- src/components/NavMain.js
+++ src/components/NavMain.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../styles/NavMain.scss'
+import '../styles/NavMain.scss';
 
 const Nav = () => {
-    return(
-        <div>
-            <div className="nav-main">
-                <div className="container">
-                    <div className="wrap-nav">
-                        <div>
-                            <NavLink exact={true} to="/">
-                                <div className="nav-logo"></div>
-                            </NavLink>
-                        </div>
-                        <div className="nav-menu">
-                            <NavLink exact to="/">About</NavLink>
-                            <NavLink to="/contact">Contact</NavLink>
-                            <NavLink to="/project">Project</NavLink>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div>
+      <div className="nav-main">
+        <div className="container">
+          <div className="wrap-nav">
+            <div>
+              <NavLink exact={true} to="/">
+                <div className="nav-logo"></div>
+              </NavLink>
             </div>
+            <div className="nav-menu">
+              <NavLink exact to="/" activeClassName="active">
+                About
+              </NavLink>
+              <NavLink to="/contact" activeClassName="active">
+                Contact
+              </NavLink>
+              <NavLink to="/project" activeClassName="active">
+                Project
+              </NavLink>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Nav;
