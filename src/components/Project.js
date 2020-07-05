acdcbc8fb54b1b6edc3586ebc33a7227cd902a16
KangEunseo <s2018w21@e-mirim.hs.kr>
2020-07-05 23:50:27 +0900

    modify project

--- src/components/Project.js
+++ src/components/Project.js
@@ -17,6 +17,7 @@ const Project = () => {
                         <div className="content-title">WORK</div>
                         {/* PROJECT LIST */}
                         <div className="wrap-project">
+                            <div className="project-image fl-right"></div>
                             <div className="project-item">
                                 <div className="project-title">RAJA</div>
                                 <div className="project-intro">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
@@ -24,7 +25,6 @@ const Project = () => {
                                     <Link to='./Raja'>View more</Link>
                                 </div>
                             </div>
-                            <div className="project-image"></div>
                         </div>
                         <div className="wrap-project">
                             <div className="project-image"></div>
@@ -37,6 +37,7 @@ const Project = () => {
                             </div>
                         </div>
                         <div className="wrap-project">
+                            <div className="project-image fl-right"></div>
                             <div className="project-item">
                                 <div className="project-title">8gram</div>
                                 <div className="project-intro">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
@@ -44,7 +45,6 @@ const Project = () => {
                                     <Link to='./8gram'>View more</Link>
                                 </div>
                             </div>
-                            <div className="project-image"></div>
                         </div>
                         <div className="wrap-project">
                             <div className="project-image"></div>
