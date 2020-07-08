2f4af38a0d509c9d7f1a4252a45d020f968040ea
yerin <ghddnjf2901@gmail.com>
2020-07-08 09:09:51 +0900

    contact test1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,7 +22,13 @@ class Contact extends Component {
   };
 
   callAPI() {
-    fetch('http://localhost:3002/contact');
+    fetch('http://localhost:3002/contact', {
+      method: 'GET',
+      headers: {
+        'access-control-allow-origin': '*',
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    }).then((results) => results.json());
   }
 
   componentWillMount() {
